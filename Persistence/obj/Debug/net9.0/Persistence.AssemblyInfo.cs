4bc45252c80c1c9c5967a9d59126bc3729b8f748
AndersPeh <pehjyfung@gmail.com>
2025-05-16 01:17:37 +1000

    API_HTTPS

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ad86140e693e340705f05db3aa35c87f9c122c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc01a6d885e794171d6caa28e39fdaa704ea8b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
