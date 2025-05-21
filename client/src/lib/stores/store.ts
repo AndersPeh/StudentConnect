3aa4cbcf55a2dc02fa69c06726f4d30e39b5a6b5
AndersPeh <pehjyfung@gmail.com>
2025-05-21 17:52:00 +1000

    MobX: isLoading state

--- client/src/lib/stores/store.ts
+++ client/src/lib/stores/store.ts
@@ -1,17 +1,24 @@
 import { createContext } from "react";
 import CounterStore from "./counterStore";
+import { UiStore } from "./uiStore";
 
 // In typescript interface, can only define the shape of an object + its properties names and types.
 // cannot assign value or instantiates classes in the interface.
 interface Store{
-    // counterStore is property of the store, a class of type CounterStore. 
-    counterStore: CounterStore
+    counterStore: CounterStore;
+
+    // uiStore is property of the store, a class of type UiStore. 
+    uiStore: UiStore;
 };
 
 // export store, a type of Store interface. can assign value here.
 export const store: Store ={
-    // create a new instance of CounterStore in counterStore.
-    counterStore: new CounterStore()
+
+    counterStore: new CounterStore(),
+
+    // create a new instance of UiStore in uiStore.
+    uiStore: new UiStore(),
+
 };
 
 // create a React Context for this store, will be used to make store instance available globally.
