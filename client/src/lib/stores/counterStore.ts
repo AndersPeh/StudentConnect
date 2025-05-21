0d12f45bb67166159c41a97a901805e913f08622
AndersPeh <pehjyfung@gmail.com>
2025-05-21 17:17:09 +1000

    MobX: Computed Property

--- client/src/lib/stores/counterStore.ts
+++ client/src/lib/stores/counterStore.ts
@@ -2,12 +2,17 @@ import {makeAutoObservable} from 'mobx';
 
 // define CounterStore class to be used in store.ts.
 export default class CounterStore {
+
+    // properties automatically become observables.
     title = 'Counter store';
     count = 40;
+    events: string[] =[
+        `Initial count is ${this.count}`
+    ];
 
     constructor(){
         // target the class (this = accessing class properties), makeAutoObservable automatically makes properties of the class (title and count observables).
-        // By observing these properties, React re-renders when there is any changes to them.
+        // By observing these properties, React re-renders when there is any changes in them.
         makeAutoObservable(this);
     }
 
@@ -16,11 +21,17 @@ export default class CounterStore {
     // must specify which observable state will be modify.
     increment = (amount = 1) =>{
         this.count += amount;
+        this.events.push(`Incremented by ${amount}. Count is now ${this.count}.`);
     }
 
     decrement = (amount = 1) =>{
         this.count -= amount;
+        this.events.push(`Decremented by ${amount}. Count is now ${this.count}.`);
     }
 
-
+    // makeAutoObservable turns get accessor into computed property. Computed property is updated when there is any changes in observables.
+    // Because the value of the computed property is derived from observables. It's cached and only changes when underlying observable changes.
+    get eventCount(){
+        return this.events.length;
+    }
 };
