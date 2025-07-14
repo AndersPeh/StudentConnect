d700da30827c8871a1b4db1ef32537466e2e796a
AndersPeh <pehjyfung@gmail.com>
2025-07-14 11:19:46 +1000

    Added HostCancelled Screenshot

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735795d822abe634dbb723b3e1318bc66422e74d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3c19d06df85e817c430200a217283b02265bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
