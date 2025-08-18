2fd22632f3de071d4e95d4dbde302975c0893508
AndersPeh <pehjyfung@gmail.com>
2025-08-18 21:29:39 +1000

    Minor Fixes and Added App Screenshots

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55880c1f27374e4a6e456c6be5acb0e275aa4287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df82b5f2f5eec9f0c6e95b28df0d8021fe015464")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,85 +1,90 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import type { LoginSchema } from "../schemas/loginSchema"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import type { LoginSchema } from "../schemas/loginSchema";
 import agent from "../api/agent";
 import { useLocation, useNavigate } from "react-router";
 import type { RegisterSchema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
 
 export const useAccount = () => {
-    // to use invalidate queries on user queryKey (to run getting user information again.)
-    const queryClient = useQueryClient();
+  // to use invalidate queries on user queryKey (to run getting user information again.)
+  const queryClient = useQueryClient();
 
-    // for navigating to other pages.
-    const navigate = useNavigate();
+  // for navigating to other pages.
+  const navigate = useNavigate();
 
-    const location = useLocation();
+  const location = useLocation();
 
-    // useMutation for handling login process.
-    const loginUser = useMutation({
+  // useMutation for handling login process.
+  const loginUser = useMutation({
     // mutationFn takes creds parameter which must be LoginSchema type (loginSchema.ts strcture), it triggers the login and state properties like isPending.
     // which means creds must contain email and password.
-        mutationFn: async(creds: LoginSchema) => {
-    // use axios to send a post request to login url and it must contain user credentials (creds).
-    // It instructs Identity endpoint to set an HttpOnly cookie in the response if the login is successful.
-    // Then subsequent API requests from the browser will include cookie.
-    // For login, we didnt make custom controller for that, just /login by Identity system is fine.
-            await agent.post('/login?useCookies=true', creds);
-        },
-        // when user logs in, automatically invalidate user queryKey so it will rerun const response = await agent.get<User>('/account/user-info');.
-        onSuccess: async() => {
-            await queryClient.invalidateQueries({
-                queryKey: ['user']
-            });
-        }
-    });
+    mutationFn: async (creds: LoginSchema) => {
+      // use axios to send a post request to login url and it must contain user credentials (creds).
+      // It instructs Identity endpoint to set an HttpOnly cookie in the response if the login is successful.
+      // Then subsequent API requests from the browser will include cookie.
+      // For login, we didnt make custom controller for that, just /login by Identity system is fine.
+      await agent.post("/login?useCookies=true", creds);
+    },
+    // when user logs in, automatically invalidate user queryKey so it will rerun const response = await agent.get<User>('/account/user-info');.
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+      toast.success("Successful Login!");
+      navigate("/activities");
+    },
+  });
 
-    // send POST request to register endpoint to register user and validate credentials and check against the database.
-    const registerUser = useMutation({
-        mutationFn: async (creds: RegisterSchema) => {
-            // For register, we made custom controller for that, need to use /account/register.
-            await agent.post('/account/register', creds);
-        },
-        onSuccess:() =>{
-            toast.success('Successful Registration: You can login now!');
-            navigate('/login');
-        }
-    })
+  // send POST request to register endpoint to register user and validate credentials and check against the database.
+  const registerUser = useMutation({
+    mutationFn: async (creds: RegisterSchema) => {
+      // For register, we made custom controller for that, need to use /account/register.
+      await agent.post("/account/register", creds);
+    },
+    onSuccess: () => {
+      toast.success("Successful Registration: You can login now!");
+      navigate("/login");
+    },
+  });
 
-    // send POST request to logout endpoint to log out and remove the user information and activities received from useQuery previously.
-    const logoutUser = useMutation({
-        mutationFn: async() => {
-            // For logout, we made custom controller for that, need to use /account/logout.
-            await agent.post('/account/logout');
-        },
-        onSuccess: () => {
-            queryClient.removeQueries({queryKey: ['user']});
-            queryClient.removeQueries({queryKey: ['activities']});
-            // automatically navigate user back to home page after logging out.
-            navigate('/');
-        }
-    })
+  // send POST request to logout endpoint to log out and remove the user information and activities received from useQuery previously.
+  const logoutUser = useMutation({
+    mutationFn: async () => {
+      // For logout, we made custom controller for that, need to use /account/logout.
+      await agent.post("/account/logout");
+    },
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: ["user"] });
+      queryClient.removeQueries({ queryKey: ["activities"] });
+      // automatically navigate user back to home page after logging out.
+      navigate("/");
+    },
+  });
 
-    // fetch and cache the current logged in user information from backend API.The result is renamed as currentUser.
-    const {data:currentUser, isLoading: loadingUserInfo} = useQuery({
-        // unique cache key which can be used to invalidated to re-run the query function below.
-        queryKey: ['user'],
-        // sends a GET request to /account/user-info endpoint and the returned response data should be User type.
-        // For user info, we made custom controller for that, need to use /account/user-info.
-        queryFn: async()=> {
-            const response = await agent.get<User>('/account/user-info');
-            return response.data;
-        },
-        // only get user information when user data doesnt exist else it will be stale and try again to get user info.
-        // if the pathname is login or register, dont send GET request to get user info.
-        enabled: !queryClient.getQueryData(['user'])  && location.pathname !== '/login' && location.pathname !== '/register'
-    })
+  // fetch and cache the current logged in user information from backend API.The result is renamed as currentUser.
+  const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
+    // unique cache key which can be used to invalidated to re-run the query function below.
+    queryKey: ["user"],
+    // sends a GET request to /account/user-info endpoint and the returned response data should be User type.
+    // For user info, we made custom controller for that, need to use /account/user-info.
+    queryFn: async () => {
+      const response = await agent.get<User>("/account/user-info");
+      return response.data;
+    },
+    // only get user information when user data doesnt exist else it will be stale and try again to get user info.
+    // if the pathname is login or register, dont send GET request to get user info.
+    enabled:
+      !queryClient.getQueryData(["user"]) &&
+      location.pathname !== "/login" &&
+      location.pathname !== "/register",
+  });
 
-    // useAccount hook returns an object that contains this 'loginUser' constant.
-    return {
-        loginUser,
-        currentUser,
-        logoutUser,
-        loadingUserInfo,
-        registerUser,
-    }
-}
+  // useAccount hook returns an object that contains this 'loginUser' constant.
+  return {
+    loginUser,
+    currentUser,
+    logoutUser,
+    loadingUserInfo,
+    registerUser,
+  };
+};
