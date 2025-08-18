ac24edd1ed109ab03c028906cd7ef49bb45aa178
AndersPeh <pehjyfung@gmail.com>
2025-08-18 17:44:38 +1000

    Create Profile Header

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc851282a7de8dc5d87082677fee5007f6790133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d68c6970ef7c8534f3c28879ec62bcb2651054")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
