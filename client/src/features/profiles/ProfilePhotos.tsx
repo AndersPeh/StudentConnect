5bfb2186a3e80aa9ec2569ab139e2a8772a71fea
AndersPeh <pehjyfung@gmail.com>
2025-09-01 19:43:44 +1000

    Modify Styling of ProfilePhoto

--- client/src/features/profiles/ProfilePhotos.tsx
+++ client/src/features/profiles/ProfilePhotos.tsx
@@ -3,6 +3,7 @@ import { useProfile } from "../../lib/hooks/useProfile";
 import {
   Box,
   Button,
+  Divider,
   ImageList,
   ImageListItem,
   Typography,
@@ -44,72 +45,82 @@ export default function ProfilePhotos() {
 
   if (loadingPhotos) return <Typography>Loading photos...</Typography>;
 
-  // Because const response = await agent.get<Photo[]>(`/profiles/${id}/photos`); returns an empty array even if there is no photo,
-  if (!photos) return <Typography>No photos found for this user.</Typography>;
-
   return (
     <Box>
-      {isCurrentUser && (
-        <Box>
-          <Button onClick={() => setEditMode(!editMode)}>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h5">Photos</Typography>
+        {isCurrentUser && (
+          <Button
+            onClick={() => setEditMode(!editMode)}
+            variant="contained"
+            color="secondary"
+          >
             {editMode ? "Cancel" : "Add Photo"}
           </Button>
-        </Box>
-      )}
+        )}
+      </Box>
+      <Divider sx={{ marginY: 2 }} />
+
       {editMode ? (
         <PhotoUploadWidget
           uploadPhoto={handlePhotoUpload}
           loading={uploadPhoto.isPending}
         />
       ) : (
-        <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
-          {photos.map((eachPhoto) => (
-            <ImageListItem key={eachPhoto.id}>
-              <img
-                // From MUI, srcSet={`${eachPhoto.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}, the query string parameters transform the image.
-                // it transforms the width and height in pixels. fit=crop means crop the image based on the height and width.
-                // auto=format sets the optimal image format according to the browser capability.
-                srcSet={`${eachPhoto.url.replace(
-                  // Perform image transformation through Cloudinary URL. c_fill fills the image according to the dimension. dpr_2 provides higher pixel images to higher resolution screen.
-                  "/upload/",
-                  "/upload/w_164,h_164,c_fill,f_auto,dpr_2/"
-                )}`}
-                src={`${eachPhoto.url.replace(
-                  // src is alternative for standard screen.
-                  "/upload/",
-                  "/upload/w_164,h_164,c_fill,f_auto/"
-                )}`}
-                alt={"User Profile Image"}
-                // Only load the image when the image is about to show up.
-                loading="lazy"
-              />
-              {isCurrentUser && (
-                <div>
-                  {/* If the user is visiting his own profile, clicking the Star button will trigger setMain for that photo. A Put Request will be sent to set that photo as profile picture. */}
-                  <Box
-                    sx={{ position: "absolute", top: 0, left: 0 }}
-                    onClick={() => setMainPhoto.mutate(eachPhoto)}
-                  >
-                    {/* If the user is visiting his own profile, if the image url matches the profile picture url, highlights the image. */}
-                    <StarButton
-                      selected={eachPhoto.url === profile?.imageUrl}
-                    />
-                  </Box>
-                  {/* Only allow user to delete non profile picture photo. */}
-                  {profile?.imageUrl !== eachPhoto.url && (
-                    <Box
-                      sx={{ position: "absolute", top: 0, right: 0 }}
-                      onClick={() => deletePhoto.mutate(eachPhoto.id)}
-                    >
-                      {/* If the user is visiting his own profile, for all images that are not the profile picture url, shows delete button. */}
-                      <DeleteButton />
-                    </Box>
+        <>
+          {photos?.length === 0 || photos == null ? (
+            <Typography>No photos added yet</Typography>
+          ) : (
+            <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
+              {photos.map((eachPhoto) => (
+                <ImageListItem key={eachPhoto.id}>
+                  <img
+                    // From MUI, srcSet={`${eachPhoto.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}, the query string parameters transform the image.
+                    // it transforms the width and height in pixels. fit=crop means crop the image based on the height and width.
+                    // auto=format sets the optimal image format according to the browser capability.
+                    srcSet={`${eachPhoto.url.replace(
+                      // Perform image transformation through Cloudinary URL. c_fill fills the image according to the dimension. dpr_2 provides higher pixel images to higher resolution screen.
+                      "/upload/",
+                      "/upload/w_164,h_164,c_fill,f_auto,dpr_2/"
+                    )}`}
+                    src={`${eachPhoto.url.replace(
+                      // src is alternative for standard screen.
+                      "/upload/",
+                      "/upload/w_164,h_164,c_fill,f_auto/"
+                    )}`}
+                    alt={"User Profile Image"}
+                    // Only load the image when the image is about to show up.
+                    loading="lazy"
+                  />
+                  {isCurrentUser && (
+                    <div>
+                      {/* If the user is visiting his own profile, clicking the Star button will trigger setMain for that photo. A Put Request will be sent to set that photo as profile picture. */}
+                      <Box
+                        sx={{ position: "absolute", top: 0, left: 0 }}
+                        onClick={() => setMainPhoto.mutate(eachPhoto)}
+                      >
+                        {/* If the user is visiting his own profile, if the image url matches the profile picture url, highlights the image. */}
+                        <StarButton
+                          selected={eachPhoto.url === profile?.imageUrl}
+                        />
+                      </Box>
+                      {/* Only allow user to delete non profile picture photo. */}
+                      {profile?.imageUrl !== eachPhoto.url && (
+                        <Box
+                          sx={{ position: "absolute", top: 0, right: 0 }}
+                          onClick={() => deletePhoto.mutate(eachPhoto.id)}
+                        >
+                          {/* If the user is visiting his own profile, for all images that are not the profile picture url, shows delete button. */}
+                          <DeleteButton />
+                        </Box>
+                      )}
+                    </div>
                   )}
-                </div>
-              )}
-            </ImageListItem>
-          ))}
-        </ImageList>
+                </ImageListItem>
+              ))}
+            </ImageList>
+          )}
+        </>
       )}
     </Box>
   );
