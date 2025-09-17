c0c660791f6e309c5de3d18f55eb059c720e35f8
AndersPeh <pehjyfung@gmail.com>
2025-09-18 01:14:38 +1000

    Update GetActivities Controller

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63748e1df472814574d1da334dc751af56ac3ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be228a862a33e0fae86a0152b1a8980b48fc9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
